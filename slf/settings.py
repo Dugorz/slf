85432af14c306c269262d834c9c9789d8646149a
Dugorz <38889200+Dugorz@users.noreply.github.com>
2019-05-23 22:27:26 +0300

    D

--- slf/settings.py
+++ slf/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z62vap!2ekk!gdzg8!#9*i$t11pn(q^j^yyw40pl2u-6of&n!e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
